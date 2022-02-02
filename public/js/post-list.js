f37abfce1b2583388f0f2c9aacd68f607beeb989
Sam James <Sam.James@softwire.com>
2022-02-02 11:15:02 +0000

    getting buttons to behave asynchronously

--- public/js/post-list.js
+++ public/js/post-list.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const likeButtons = document.querySelectorAll('.like-click');
     const dislikeButtons = document.querySelectorAll('.dislike-click');
-
     // console.log(likeButton, dislikeButton);
     likeButtons.forEach(button => {
         button.addEventListener('click', () => {
         button.classList.add('clicked-like');
         button.innerHTML = 'You liked this post';
+        updateLikes();
         });
     });
     dislikeButtons.forEach(button => {
@@ -16,4 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
     button.innerHTML = 'You disliked this post';
         });
     });
+
+    async function updateLikes(){
+        await fetch(`/${id}/like/`);
+    }
+
+
 });
